a9387987fb8fa125688d600b8fd962d22c16220c
masakiShito <98877813+masakiShito@users.noreply.github.com>
2023-04-03 22:48:08 +0900

    【サーバサイド】SpringSecurityの設定内容変更
    【サーバサイド】SpringSecurityの設定内容変更
    変更内容
    ・CSSファイル、jsファイル、imgファイルのアクセス制御を廃止

--- src/main/java/com/householdaccountbook/config/SecurityConfig.java
+++ src/main/java/com/householdaccountbook/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.householdaccountbook.config;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -30,5 +31,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService)
 				.passwordEncoder(NoOpPasswordEncoder.getInstance());
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// 静的リソース(js、css、img)に対するアクセスはセキュリティ設定を無視する
+		web.ignoring().antMatchers("/js/**", "/css/**", "/img/**");
 	}
 }
